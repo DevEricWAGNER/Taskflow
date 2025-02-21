87eb81b6484603d2e781a0a41b3da1d34f9b11b4
DevEricWAGNER <ericwagner.contact@gmail.com>
2025-02-21 16:55:47 +0000

    Add Navbar and Wrapper components for improved layout and navigation

---
+++ app/components/NavBar.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import { UserButton } from '@clerk/nextjs';
+import { FolderGit2, Menu, X } from 'lucide-react';
+import Link from 'next/link';
+import React, { useState } from 'react';
+
+const Navbar = () => {
+
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const navLinks = [
+        {
+            href: "/general-projects", label: "Collaborations"
+        },
+        {
+            href: "/", label: "Mes projets"
+        }
+    ]
+
+    const renderLinks = (classNames : string) => {
+        return navLinks.map(({href, label}) => {
+            return <Link key={href} href={href} className={`${classNames} btn-sm`}>
+                    {label}
+                </Link>
+        })
+    }
+
+  return (
+    <div className='border-b border-base-300 px-5 md:px-[10%] py-4 relative'>
+        <div className='flex justify-between items-center'>
+            <div className='flex items-center'>
+                <div className='bg-primary-content text-primary rounded-full p-2'>
+                    <FolderGit2 className='w-6 h-6'/>
+                </div>
+                <span className='ml-3 font-bold text-3xl'>
+                    Task <span className='text-primary'>Flow</span>
+                </span>
+            </div>
+            <button className='btn w-fit btn-sm sm:hidden' onClick={() => setMenuOpen(!menuOpen)}>
+                <Menu className='w-4'/>
+            </button>
+            <div className='hidden sm:flex space-x-4 items-center'>
+                {renderLinks("btn")}
+                <UserButton />
+            </div>
+
+            {/* menu responsive */}
+            <div className={`absolute top-0 w-full h-screen flex flex-col gap-2 p-4 transition-all duration-300 sm:hidden bg-white z-50 ${menuOpen ? 'left-0' : '-left-full'}`}>
+                <div className='flex justify-between'>
+                    <UserButton />
+                    <button className='btn w-fit btn-sm' onClick={() => setMenuOpen(!menuOpen)}>
+                        <X className='w-4'/>
+                    </button>
+                </div>
+                {renderLinks("btn")}
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default Navbar;
